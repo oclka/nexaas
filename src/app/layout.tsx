f789341665911215cbbfc634e16cd9db31b565b3
OCLKA <oclcode@outlook.com>
2026-04-10 11:52:20 +0200

    feat(setup): add prettier config and update gitignore
    - Add `.prettierignore` and `.prettierrc` files for code formatting
    - Update `.gitignore` to include additional directories and files
    - Switch from double quotes to single quotes in various config files
    - Add prettier as a dependency and script in `package.json`
    - Update import statements in multiple files to use single quotes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
-import "@/ui/globals.css";
-import { inter } from "@/ui/fonts";
-import { app } from "@/core/config";
+import type { Metadata } from 'next';
+import '@/ui/globals.css';
+import { inter } from '@/ui/fonts';
+import { app } from '@/core/config';
 
 export const metadata: Metadata = {
   title: app.name,
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
--- src/core/config/index.ts
+++ src/core/config/index.ts
@@ -1,6 +1,6 @@
 export const app = {
-  name: "nexaas",
+  name: 'nexaas',
   description:
-    "A scalable, production-ready SaaS boilerplate built for high performance and rapid prototyping in Next.js 16.",
-  logo: "/images/oclka_logo.svg",
+    'A scalable, production-ready SaaS boilerplate built for high performance and rapid prototyping in Next.js 16.',
+  logo: '/images/oclka_logo.svg',
 };
